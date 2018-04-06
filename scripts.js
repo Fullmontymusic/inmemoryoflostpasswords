e0a776fc414f415cc01a627dff6367fc887c4bbd
Boris Boom <boris@Boriss-MacBook-Pro.local>
2018-04-06 14:10:33 +0200

    Initial version

---
+++ scripts.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+    // all custom jQuery will go here
+    // Can also be included with a regular script tag
+
+
+  var options = {
+    strings: [
+      "Eddie070",
+      "Passw123!",
+      "OP7x7yl",
+      "thisiskelly2",
+      "sdofi78",
+      "Rmmbr94!",
+      "CxZi7pd",
+      "Misskitt3n",
+      "Chocolat3",
+      "Tobias55",
+      "V!spunt90",
+      "R7XloPa",
+      "Bacon88",
+      "LisaX0X0",
+      "Apple06",
+      "WbiU3u8",
+      "9JsdF6",
+      "TonyR96",
+      "FamPass6",
+      "JQpj9E7",
+      "Savecash18",
+      "Instapass2",
+      "Stprnd69",
+      "WifiPass01",
+      "adminXS",
+      "XDiu17v",
+      "Love4youX",
+      "Kimberly3",
+      "Tweet018",
+
+
+    ],
+    typeSpeed: 90,
+    backDelay: 800,
+    backSpeed: 500,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+    fadeOut: true,
+  }
+
+  var typed = new Typed(".element", options);
+
+});
+
+$(document).ready(function () {
+    $('.menu').delay(1800).fadeIn(1000);
+    // OR $('div.toshow').show(2200);
+    // OR $('div.toshow').slideDown("slow");
+});
+
+$(document).ready(function () {
+    $('.footer').delay(1800).fadeIn(1000);
+    // OR $('div.toshow').show(2200);
+    // OR $('div.toshow').slideDown("slow");
+});
+
+<!-- werkt nu niet, want geen display none niet in html gedaan
+$(document).ready(function () {
+    $('.forgotten-passwords').fadeIn(1000);
+    // OR $('div.toshow').show(2200);
+    // OR $('div.toshow').slideDown("slow");
+});
+-->
